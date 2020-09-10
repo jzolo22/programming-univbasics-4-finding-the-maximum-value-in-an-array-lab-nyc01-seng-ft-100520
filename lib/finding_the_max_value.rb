647966ff8ac3c2927a2dc029cbd6dea81299ffa5
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-10 16:57:26 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -9,3 +9,20 @@ def find_max_value(array)
   end
   return max_value
 end
+
+
+# ORIGINAL SOLUTION BEFORE MAKING IT MORE LIKE THE READABLE ONE IN THE LESSON
+# def find_max_value(array)
+#   i = 0
+#   max_value = 0
+#   length = array.length
+#   while i < length do
+#     if array[i] < max_value
+#       i += 1
+#     else
+#       max_value = array[i]
+#       i += 1
+#     end
+#   end
+#   return max_value
+# end
